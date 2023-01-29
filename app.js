aae3f6342e25948bd37ac08891b6ce872356938e
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-30 00:19:37 +0530

    use router set engine serve public dir

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const userRouter = require('./routes/user');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('server running');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use('/', userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on PORT:${process.env.PORT}`);
