7d3baef69d5e885673cc7aa1dd85f11052f364e7
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-30 01:22:15 +0530

    send home page status

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,14 @@
 // PAGE
 exports.getLandingPage = (req, res, next) => {
-    return res.status(200).render('user/landing-page');
+    return res.status(200).render('user/landing-page', {
+        home_page: true
+    });
 }
 
 exports.getClaimForm = (req, res, next) => {
-    return res.status(200).render('user/claim-form');
+    return res.status(200).render('user/claim-form', {
+        home_page: false
+    });
 }
 
 // API
