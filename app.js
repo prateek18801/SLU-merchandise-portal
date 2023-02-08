939683dcae2b85c6c2089c550f174bff75d6c658
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-08 11:01:41 +0530

    use auth admin routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+const adminRouter = require('./routes/admin');
 require('./utils/db').connect();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.use('/', userRouter);
+app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on PORT:${process.env.PORT}`);
